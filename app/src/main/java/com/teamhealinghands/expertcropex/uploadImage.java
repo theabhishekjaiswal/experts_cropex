49ccbcd26eef3a082427fa7a7cd6d2e8be47fb77
The Abhishek Jaiswal <abhivishaljaisdwal420@gmail.com>
2023-09-23 12:37:06 +0530

    final commit

---
+++ app/src/main/java/com/teamhealinghands/expertcropex/uploadImage.java
@@ -0,0 +1,14 @@
+package com.teamhealinghands.expertcropex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uploadImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_upload_image);
+    }
+}
